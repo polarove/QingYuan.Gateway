49500d5ec26a6ac5f62b7d20eedb78d01b2e3a24
刘棋 <liuqi6602@163.com>
2025-03-11 18:01:44 +0800

    asf

---
+++ QingYuan.Common/Encrypt/Salt.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+namespace QingYuan.Common.Encrypt
+{
+    public class Salt
+    {
+        public static string Generate(int size = 16)
+        {
+            var bytes = new byte[size];
+            RandomNumberGenerator.Fill(bytes); // 用随机数据填充字节数组
+            byte[ ] hashBytes = SHA256.HashData(bytes);
+            string hashString = Convert.ToHexString(hashBytes).ToLower();
+            return hashString;
+        }
+    }
+}
---
+++ QingYuan.Test/Test1.cs
@@ -0,0 +1,15 @@
+﻿using QingYuan.Common.Encrypt;
+
+namespace QingYuan.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var hash = Salt.Generate();
+            Console.WriteLine(hash);
+        }
+    }
+}
