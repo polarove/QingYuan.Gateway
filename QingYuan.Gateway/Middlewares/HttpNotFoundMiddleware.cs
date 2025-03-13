ac11996b89a2f5b5386ddafae71cc0635e0d30d3
刘棋 <liuqi6602@163.com>
2025-03-13 18:34:52 +0800

    asf

--- QingYuan.Gateway/Middlewares/HttpNotFoundMiddleware.cs
+++ QingYuan.Gateway/Middlewares/HttpNotFoundMiddleware.cs
@@ -3,20 +3,23 @@ using QingYuan.Mvc.Enums;
 
 namespace QingYuan.Gateway.Middlewares
 {
-    public class HttpNotFoundMiddleware(RequestDelegate next)
+    public class HttpNotFoundMiddleware(RequestDelegate next, IWebHostEnvironment env)
     {
-        private readonly RequestDelegate _next = next;
 
         public Task Invoke(HttpContext context)
         {
             var endPoint = context.GetEndpoint();
             if (endPoint != null)
             {
-                return _next(context);
+                return next(context);
+            }
+            else if (env.IsDevelopment())
+            {
+                context.Response.Redirect("/openapi/v1.json");
+                return Task.CompletedTask;
             }
             context.Response.StatusCode = 404;
             return context.Response.WriteAsJsonAsync(ApiResponseResult.Fail(EnumApiResponseResultCode.NotFound, "未找到服务"));
         }
-
     }
 }
